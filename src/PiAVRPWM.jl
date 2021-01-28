413bf85507918021433eabbcc4e820ad7d62a3b0
notinaboat <notinaboat@icloud.com>
2021-01-28 15:32:58 +1100

    working but too slow

--- src/PiAVRPWM.jl
+++ src/PiAVRPWM.jl
@@ -1,5 +1,34 @@
+"""
+# PiAVRPWM.jl
+
+17 Channel PWM output using ATMega328p.
+"""
 module PiAVRPWM
 
-greet() = print("Hello World!")
+export AVRPWM
+
+using PiAVR
+
+const pwm_max = begin
+    main_c = joinpath(@__DIR__, "main.c")
+    max = read(`awk '/#define PWM_MAX/ {print$3}' $main_c`, String) |> chomp
+    parse(Float64, max)
+end
+
+struct AVRPWM
+    avr::AVRDevice
+    AVRPWM(; args...) =
+        new(AVRDevice(;c_code = joinpath(@__DIR__, "main.c"), args...))
+end
+
+function set_pwm_level(pwm, channel, level)
+    @assert level >= 0.0 && level <= 1.0
+    l = round(UInt16, level * pwm_max)
+    write(pwm.avr, UInt8[channel,
+                         0b01000000 | l >> 6,
+                         0b10000000 | l & 0b00111111])
+end
+
+Base.setindex!(pwm::AVRPWM, level, channel) = set_pwm_level(pwm, channel, level)
 
 end # module
--- src/main.c
+++ src/main.c
@@ -2,33 +2,6 @@
 
 17-channel soft-PWM.
 
-
-                 ATMega328p "Nano"
-             ┌───────────────────────┐
-             │      RST SCK MISO     │
-             │      [ ] [ ] [ ]      │
-             │      [ ] [ ] [ ]      │
-             │      GND MOSI 5V      │
-             │                       │          
-       PWM1  │[ ] TX  D1      VIN [ ]│
-       PWM0  │[ ] RX  D0      GND [ ]│ 
-             │[ ] RST      C6 RST [ ]│  (RST)
-             │[ ] GND          5V [ ]│
-       PWM2  │[ ] D2  D2       A7 [ ]│
-       PWM3  │[ ] D3  D3       A6 [ ]│
-       PWM4  │[ ] D4  D4   C5  A5 [ ]│  PWM16
-       PWM5  │[ ] D5  D5   C4  A4 [ ]│  PWM15
-       PWM6  │[ ] D6  D6   C3  A3 [ ]│  PWM14
-       PWM7  │[ ] D7  D7   C2  A2 [ ]│  PWM13
-       PWM8  │[ ] D8  B0   C1  A1 [ ]│  PWM12
-       PWM9  │[ ] D9  B1   C0  A0 [ ]│  PWM11
-      PWM10  │[ ] D10 B2      REF [ ]│
-     (MOSI)  │[ ] D11 B3      3V3 [ ]│
-     (MISO)  │[ ] D12 B4   B5 D13 [ ]│  (SCK)
-             │      ┌─────────┐      │
-             └──────┤   USB   ├──────┘
-                    │         │
-                    └─────────┘
 */
 
 // Monitor Serial Data TX pin.
@@ -52,82 +25,123 @@
     void print_channel(uint8_t channel);
 #endif
 
-#define PWM_BUF_LEN 64
-#define PWM_MAX (PWM_BUF_LEN * 8)
-#define PWM_CHANNEL_COUNT 17
+#define PWM_MAX 512
+#define PWM_BUF_LEN (PWM_MAX / 8)
 
 
 // SPI RX.
+#ifndef UNIT_TEST
 #define SPI_MOSI        pin_code(PORTB, 3)
 #define SPI_MISO        pin_code(PORTB, 4)
 #define SPI_SCK         pin_code(PORTB, 5)
-
-const uint8_t pwm_pins[] = {
-    pin_code(PORTD, 0), //  0
-    pin_code(PORTD, 1), //  1
-    pin_code(PORTD, 2), //  2
-    pin_code(PORTD, 3), //  3
-    pin_code(PORTD, 4), //  4
-    pin_code(PORTD, 5), //  5
-    pin_code(PORTD, 6), //  6
-    pin_code(PORTD, 7), //  7
-    pin_code(PORTB, 0), //  8
-    pin_code(PORTB, 1), //  9
-    pin_code(PORTB, 2), // 10
-    pin_code(PORTC, 0), // 11
-    pin_code(PORTC, 1), // 12
-    pin_code(PORTC, 2), // 13
-    pin_code(PORTC, 3), // 14
-    pin_code(PORTC, 4), // 15
-    pin_code(PORTC, 5), // 16
-};
+#include "bbspi.c"
+#endif
 
 
-#ifndef UNIT_TEST
-// SPI bit-bang RX.
-volatile uint8_t rx_t0;
-volatile uint8_t rx_i;
-volatile uint8_t rx_buf;
-#define RX_TIMEOUT 2
+#include "pwm_pins.h"
 
-ISR(PCINT0_vect)
+
+// PWM output level setting.
+static int16_t pwm_level[PWM_PIN_COUNT];
+
+// PWM bit patterns.
+static uint8_t pwm_buf[PWM_PIN_COUNT][PWM_BUF_LEN];
+
+// PWM bit pattern cursor.
+static uint8_t pwm_i;
+static uint8_t pwm_mask;
+
+// Configure GPIO pins for output and zero bit patterns.
+static void pwm_init()
 {
-    if (pin_is_high(SPI_SCK)) {
-        if (rx_i == 0 || (_rtc - rx_t0) > RX_TIMEOUT) {
-            rx_buf = 0;
-            rx_i = 0;
-            rx_t0 = _rtc;
+    for (uint8_t i = 0 ; i < PWM_PIN_COUNT ; i++) {
+        pin_enable_output(pwm_pins[i]);
+        pin_set(pwm_pins[i], 0);
+        pwm_level[i] = 0;
+        for (uint8_t j = 0 ; j < PWM_BUF_LEN ; j++) {
+            pwm_buf[i][j] = 0;
         }
-        if (rx_i < 8) {
-            uint8_t b = rx_buf;
-            if (pin_is_high(SPI_MOSI)) {
-                b |= 1;
-            }
-            rx_i++;
-            if (rx_i < 8) {
-                b <<= 1;
-            }
-            rx_buf = b;
-        }
+    }
+    pwm_i = 0;
+    pwm_mask = 1;
+}
+
+
+// Move cursor to next position.
+static void increment_pwm()
+{
+    pwm_mask <<= 1;
+    if (pwm_mask == 0) {
+        pwm_mask = 1;
+        pwm_i ++;
+    }
+    if (pwm_i == PWM_BUF_LEN) {
+        pwm_i = 0;
     }
 }
 
-static void bbspi_init()
+
+// PWM state for `channel` at current cursor position.
+static uint8_t pwm_state(uint8_t channel)
 {
-    rx_i = 0;
-    rx_buf = 0;
-
-    // Configure SPI RX pins...
-    pin_enable_input(SPI_SCK);
-    pin_disable_pullup(SPI_SCK);
-    pin_enable_input(SPI_MOSI);
-    pin_disable_pullup(SPI_MOSI);
-
-    // Connect PORTB bit 5 to PCINT0_vect
-    PCMSK0 |= (1 << PCINT5);                     // [ATMega328.pdf, 10.2.8, p74]
-    PCICR |= (1 << PCIE0);                       // [ATMega328.pdf, 10.2.4, p73]
+    return pwm_buf[channel][pwm_i] & pwm_mask;
 }
-#endif
+
+
+// Write PWM state for each channel to the GPIO pins.
+static void pwm_output()
+{
+    for (uint8_t i = 0 ; i < PWM_PIN_COUNT ; i++) {
+        pin_set(pwm_pins[i], pwm_state(i));
+    }
+    increment_pwm();
+}
+
+
+// Set bit `i` in a `channel`'s PWM buffer to `value` (0 or 1).
+// Return `1` if the bit was changed from `0` to `1`.
+// Return `-1` if the bit was changed from `1` to `0`.
+// Return `0` if the bit was unchanged.
+static int8_t pwm_set_bit(uint8_t channel, uint16_t i, uint8_t value)
+{
+    assert(channel < PWM_PIN_COUNT);
+    assert(i < PWM_MAX);
+    int8_t result = 0;
+    uint8_t mask = 1 << (i % 8);
+    i >>= 3;
+    uint8_t byte = pwm_buf[channel][i];
+    if (value) {
+        if ((byte & mask) == 0) {
+            byte |= mask;
+            result = 1;
+        }
+    } else {
+        if (byte & mask) {
+            byte &= ~mask;
+            result = -1;
+        }
+    }
+    pwm_buf[channel][i] = byte;
+    return result;
+}
+
+
+// Set a random bit in the PWM pattern buffer for `channel`.
+static void pwm_set_random_bit(uint8_t channel)
+{
+    assert(channel < PWM_PIN_COUNT);
+
+    uint32_t i = rand() % PWM_MAX;
+    for (;;) {
+        if (pwm_set_bit(channel, i, 1)) {
+            return;
+        }
+        i++;
+        if (i == PWM_MAX) {
+            i = 0;
+        }
+    }
+}
 
 
 // Value at index `i` is the number of bits set in binary representation of `i`.
@@ -141,177 +155,57 @@ static int8_t bit_count(uint8_t v)
 }
 
 
-static int16_t pwm_level[PWM_CHANNEL_COUNT];
-static uint8_t pwm_buf[PWM_CHANNEL_COUNT][PWM_BUF_LEN];
-
-static uint8_t pwm_i;
-static uint8_t pwm_mask;
-
-static void pwm_init()
-{
-    for (uint8_t i = 0 ; i < PWM_CHANNEL_COUNT ; i++) {
-        pin_enable_output(pwm_pins[i]);
-        pin_set(pwm_pins[i], 0);
-        pwm_level[i] = 0;
-        for (uint8_t j = 0 ; j < PWM_BUF_LEN ; j++) {
-            pwm_buf[i][j] = 0;
-        }
-    }
-    pwm_i = 0;
-    pwm_mask = 1;
-}
-
-static void increment_pwm()
-{
-    pwm_mask <<= 1;
-    if (pwm_mask == 0) {
-        pwm_mask = 1;
-        pwm_i ++;
-    }
-    if (pwm_i == PWM_BUF_LEN) {
-        pwm_i = 0;
-    }
-}
-
-static void set_pwm_bit(uint8_t channel, uint16_t i, uint8_t value)
-{
-    uint8_t mask = 1 << (i % 8);
-    i >>= 4;
-    if (value) {
-        pwm_buf[channel][i] |= mask;
-    } else {
-        pwm_buf[channel][i] &= ~mask;
-    }
-}
-
-static uint8_t pwm_state(uint8_t channel)
-{
-    return pwm_buf[channel][pwm_i] & pwm_mask;
-}
-
-static void pwm_output()
-{
-    for (uint8_t i = 0 ; i < PWM_CHANNEL_COUNT ; i++) {
-        pin_set(pwm_pins[i], pwm_state(i));
-    }
-    increment_pwm();
-}
-
-static int8_t pwm_random_tweak(uint8_t channel, uint8_t tweak_upwards)
-{
-    assert(channel < PWM_CHANNEL_COUNT);
-
-    // Random index and mask.
-    uint16_t r = rand();
-    uint8_t i = r % PWM_BUF_LEN;
-    uint8_t mask = r >> 8;
-
-    // Count the number of one bits in byte.
-    uint8_t byte = pwm_buf[channel][i];
-    int8_t b_count = bit_count(byte);
-
-    // Tweak the byte.
-    if (tweak_upwards) {
-        byte |= mask;
-    } else {
-        byte &= mask;
-    }
-    pwm_buf[channel][i] = byte;
-
-    // Count the number bits added or removed.
-    int8_t result = bit_count(byte) - b_count;
-    if (tweak_upwards) {
-        assert(result >= 0);
-    } else {
-        assert(result <= 0);
-    }
-    return result;
-}
-
-static uint8_t reverse_bits(uint8_t byte) 
-{ 
-    return ((byte >> 1) & 0x55) | ((byte << 1) & 0xAA); 
-}
-
-static void pwm_tweak(uint8_t channel, int16_t err)
-{
-    for (uint8_t i = 0 ; i < PWM_BUF_LEN && err != 0 ; i++) {
-        uint8_t j = reverse_bits(i);
-        uint8_t byte = pwm_buf[channel][j];
-        int8_t b_count = bit_count(byte);
-        uint8_t mask = 1;
-        while(b_count < 8 && err > 0 && mask != 0) {
-            if ((byte & mask) == 0) {
-                byte |= mask;
-                b_count++;
-                err--;
-            }
-            mask <<= 1;
-        }
-        while(b_count > 0 && err < 0 && mask != 0) {
-            if ((byte & mask) != 0) {
-                byte &= ~mask;
-                b_count--;
-                err++;
-            }
-            mask <<= 1;
-        }
-        pwm_buf[channel][j] = byte;
-    }
-}
-
-
+// Set PWM `channel` to `target` output value.
 static void pwm_set(uint8_t channel, uint16_t target)
 {
     assert(target <= PWM_MAX);
-    assert(channel < PWM_CHANNEL_COUNT);
+    assert(channel < PWM_PIN_COUNT);
 
-    int16_t level = pwm_level[channel];
-
-    // How far from the target is the current level?
-    int16_t err = target - level;
-    if (err == 0) {
-        return;
-    }
-
-    // Apply limited number of random tweaks.
-    uint16_t tweak_limit = abs(err/2);
-    //printn("tweak_limit:", tweak_limit);
-    for (uint8_t i = 0 ; i < tweak_limit ; i++) {
-        level += pwm_random_tweak(channel, level < target);
-        if (level == target || (err > 0 && level > target)) {
+    // Find a ratio `a:b` that approximates `target:PWM_MAX`...
+    uint16_t a = target;
+    uint16_t b = PWM_MAX;
+    int16_t err = 0;
+    while(a > 1) {
+        uint16_t _a = a / 2;
+        uint16_t _b = b / 2;
+        err = target - _a * (PWM_MAX / _b);
+        if (err > (PWM_MAX / _b)) {
             break;
         }
+        a = _a;
+        b = _b;
+    }
+    assert(err >= 0);
+
+    // Create a bit pattern using the ratio `a:b`...
+    for (uint16_t i = 0 ; i < PWM_MAX ; i ++) {
+        pwm_set_bit(channel, i, (i % b) < a);
     }
 
-    // Correct the remaining error.
-    pwm_tweak(channel, target - level);
+    // Fill in the remaining bits randomly...
+    while (err--) {
+        pwm_set_random_bit(channel);
+    }
 
-    // Check that target was reached.
-    int16_t count = 0;
+    // Check that the target was reached.
+    uint16_t count = 0;
     for (uint8_t i = 0 ; i < PWM_BUF_LEN ; i++) {
         count += bit_count(pwm_buf[channel][i]);
     }
-    //printn("count: ", count);
-    //printn("target: ", target);
     assert(count == target);
 
     pwm_level[channel] = target;
-    printn("target: ", target);
-}
+    printn("err: ", target - count);
 
+    return;
+}
 
 
 #ifndef UNIT_TEST
 
-uint16_t counter = 0;
-
-ISR(TIMER2_COMPB_vect)
+ISR(TIMER1_COMPA_vect)
 {
-//    counter++;
-//    if (counter % 5 == 0) {
-        pwm_output();
-//    }
+    pwm_output();
 }
 
 int main(void) __attribute((noreturn));
@@ -328,10 +222,14 @@ int main()
 
     pwm_init();
 
-    // Enable TIMER2_COMPB ISR...
-    OCR2B = 100;
-    TIFR2 |= (1 << OCF2B);
-    TIMSK2 |= (1 << OCIE2B);
+    // CTC mode, 16MHz / 8 = 2000kHz...
+    TCCR1A = 0;
+    TCCR1B = (1 << WGM12) | (1 << CS11) | (0 << CS10);
+     
+    // Enable TIMER1_COMPA ISR...
+    OCR1A = 200;
+    TIFR1 |= (1 << OCF1A);
+    TIMSK1 |= (1 << OCIE1A);
 
     // Enable interrupts..
     sei();
@@ -339,31 +237,20 @@ int main()
     uint8_t channel = 0;
     uint16_t target = 0;
 
-    println("Hello!!");
+    println("PiAVRPWM Started.");
 
     for(;;) {
-
-        if (rx_i == 8) {
-            cli();
-                uint8_t byte = rx_buf;
-                rx_i = 0;
-            sei();
-            //printn("byte: ", byte);
-            uint8_t flag = (byte & 0b11000000) >> 6;
-            //printn("flag: ", flag);
-            byte &= 0b00111111;
-            if (flag == 0) {
-                channel = byte;
-            } else if (flag == 1) {
-                target = (uint16_t)byte << 6;
-            } else if (flag == 2) {
-                target |= byte;
-                //printn("channel: ", channel);
-                //printn("target: ", target);
-                pwm_set(channel, target);
-            }
+        uint8_t byte = bbspi_rx();
+        uint8_t flag = (byte & 0b11000000) >> 6;
+        byte &= 0b00111111;
+        if (flag == 0) {
+            channel = byte;
+        } else if (flag == 1) {
+            target = (uint16_t)byte << 6;
+        } else if (flag == 2) {
+            target |= byte;
+            pwm_set(channel, target);
         }
-
     }
 }
 
@@ -376,9 +263,11 @@ int main()
 void print_channel(uint8_t channel)
 {
     int16_t count = 0;
-    for (uint8_t i = 0 ; i < PWM_BUF_LEN ; i += 4) {
+    for (uint8_t i = 0 ; i < PWM_BUF_LEN ; i++) {
         uint8_t byte = pwm_buf[channel][i];
-        printf("%02X ", byte);
+        if (i % 4 == 0) {
+            printf("%02X ", byte);
+        }
         count += bit_count(byte);
     }
     printf("(%d/%d)\n", count, PWM_MAX);
